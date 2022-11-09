f01b4e8ebd2ed0ffce1cde4f8a21401f70c36429
monometa <il.kaltovich@gmail.com>
2022-11-09 15:07:51 +0300

    fix: transfer header params to session_f

--- parser_config.py
+++ parser_config.py
@@ -41,23 +41,15 @@ list_of_dates_2020 = [
     "Вторник, 01 Декарь, 2020",
 ]
 
-url = "https://115.xn--90ais/portal/wwv_flow.ajax"
-# dymanic params for header:
-cookie = "OracleMapsPlugin_R761175865244051364_7119351139895=16#3088804855624837#7158895191585507#3857; PORTAL_USER_TOKEN=A1D77B502D160AE2C3873E6B39E222D5D0ED10354CDFA10DFAF2A65DF50717F353B04BD473619F3DC851F6A2F94A7F16ED92CA18B54C49758005B5A7EBFC0699; OracleMapsPlugin_R761175865244051364_11018758419082=16#3121932443289751#7171117959163840#3857; ORA_WWV_APP_10901=ORA_WWV-u1YCkIOAGSnp3yaHnj8xyCCa; ORA_WWV_RAC_INSTANCE=2; ai_user=yIAIFgz6SRFD8GLE51HHnU|2022-11-02T09:35:41.515Z"
-referer = (
-    "https://115.xn--90ais/portal/f?p=10901:REQUESTS_MAP:115927250491320::NO:::"
-)
-
-
 week_days = {
-        "1": "Понедельник",
-        "2": "Вторник",
-        "3": "Среда",
-        "4": "Четверг",
-        "5": "Пятница",
-        "6": "Суббота",
-        "0": "Воскресенье",
-    }
+    "1": "Понедельник",
+    "2": "Вторник",
+    "3": "Среда",
+    "4": "Четверг",
+    "5": "Пятница",
+    "6": "Суббота",
+    "0": "Воскресенье",
+}
 
 months_translate = {
     "January": "Январь",
@@ -73,4 +65,3 @@ months_translate = {
     "November": "Ноябрь",
     "December": "Декарь",
 }
-
