38d2b7e5bf91a594db330771e916143dc16a203d
monometa <il.kaltovich@gmail.com>
2022-11-22 12:25:05 +0300

    fix: add definition for working part

--- airflow/dags/submit_2_spark.py
+++ airflow/dags/submit_2_spark.py
@@ -2,36 +2,77 @@ import os
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
+from airflow.operators.bash import BashOperator
+from airflow.providers.amazon.aws.transfers.local_to_s3 import (
+    LocalFilesystemToS3Operator,
+)
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-# from airflow.hooks.S3_hook import S3Hook
-from airflow.models.connection import Connection
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+from pathlib import Path
+from pyspark.sql import SparkSession
 
 default_args = {
-    'owner': 'Airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
+    "owner": "Airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2015, 6, 1),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
     # 'retry_delay': timedelta(minutes=5),
 }
 
-spark_dag = DAG(
-        dag_id = "spark_airflow_dag",
-        default_args=default_args,
-        # schedule_interval=None,	
-        schedule_interval="@once",
-        # dagrun_timeout=timedelta(minutes=60),
-        description='use case of sparkoperator in airflow',
-        # start_date = datetime(2022, 1, 1)
-        )
-Extract = SparkSubmitOperator(
-		application ='./scripts/tranform_2_parquet.py',
-		conn_id= 'SparkLocal', 
-		task_id='spark_submit_task', 
-		dag=spark_dag
-		)
-Extract
+# //////////////////////////////// PythonOperator - works successfully 
+
+# def transform_spark():
+
+#     path = Path(".")
+#     path_resources = path / "resources"
+#     spark = SparkSession.builder.appName("bel").getOrCreate()
+
+#     raw_json_dataframe = (
+#         spark.read.format("json")
+#         .option("inferSchema", "true")
+#         .load("/usr/local/spark/resources/July.json")
+#     )
+
+#     raw_json_dataframe.printSchema()
+
+#     (raw_json_dataframe.write.options(header="True", delimiter=",")
+#         .csv(f"{path_resources}/test_csv.csv")
+#     )
+
+# with DAG(
+#     dag_id="spark_airflow_dag",
+#     default_args=default_args,
+#     # schedule_interval=None,
+#     schedule_interval="@once",
+#     # dagrun_timeout=timedelta(minutes=60),
+#     description="use case of sparkoperator in airflow",
+#     # start_date = datetime(2022, 1, 1)
+# ) as dag:
+#     extract = PythonOperator(
+#         task_id="elt_documento_pagar_spark", python_callable=transform_spark
+#     )
+
+# (extract)
+
+# ////////////////////////////////
+
+# //////////////////////////////// SparkSubmitOperator - fails
+
+with DAG(
+    dag_id="spark_airflow_dag",
+    default_args=default_args,
+    schedule_interval="@once",
+) as dag:
+
+    transform_2_csv = SparkSubmitOperator(
+        application="/usr/local/spark/app/transform_2_csv.py",
+        conn_id="SparkLocal",
+        task_id="spark_submit_task",
+    )
+(transform_2_csv)
+
+# ////////////////////////////////
